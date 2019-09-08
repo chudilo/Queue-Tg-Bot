d13af496d4007d556ba9abda8511c5ad2b098ff7
chudilo <chudik97@yandex.ru>
2019-09-08 13:36:17 +0300

    Added simle logic for help and setcount, not tested

--- main.py
+++ main.py
@@ -2,88 +2,39 @@ import requests
 import json
 import time
 
-URL = 'https://api.telegram.org/bot'
-with open('token', 'r') as f:
-    URL += f.read().strip()
-
-
-def getUpdates(update_id=1):
-    url = URL + '/getUpdates?offset=' + str(update_id)
-    r = requests.get(url)
-    return r.json()
-
-
-def sendMessage(chat_id, text="Test message"):
-    url = URL + '/sendMessage'
-    r = requests.post(url, json={'chat_id': chat_id, 'text': text})
-    return r.json()
-
-
-def getMe():
-    url = URL + '/getMe'
-    r = requests.get(url)
-    return r.json()
-
-
-def answerMessage(message, info):
-    msg_txt = message['message']['text']
-    
-    if '/start' in msg_txt:
-        #Предложить выбрать никнейм
-        pass
-    
-    elif '/help' in msg_txt:
-        #Вывести список команд
-	pass
-    
-    elif '/come' in msg_txt:
-	#Добавить в список
-	pass
-
-    elif '/leave' in msg_txt:
-	#Убрать из списка
-	pass
-
-
-    elif '/info' in msg_txt:
-	#Вывести информацию об очередях
-	pass
-
-
-    elif '/
-    
+from mytgapi import getMe, getUpdates, sendMessage, answerMessage
 
 
 def main():
-    people_at_location = None
+    count_of_people = None
     update_id = None
     inf = None
-    
+
     with open('info.conf', 'r') as f:
         inf = json.load(f)
-        people_at_location = inf['people_at_location']
+        count_of_people = inf['count_of_people']
         update_id = inf['update_id']
-        
+
     while True:
         resp = getUpdates(update_id)
         print(json.dumps(resp, indent=4))
         if resp['result']:
             for message in resp['result']:
                 #text = 'Ты написал мне ' + message['message']['text'].strip().lower()
-                answer = answerMessage(message, inf)
+                answer, inf = answerMessage(message, inf)
                 sendMessage(message['message']['chat']['id'], answer)
-            
+
             update_id = resp['result'][-1]['update_id'] + 1
 
             with open('info.conf', 'w') as f:
                 inf['update_id'] = update_id
-                json.dump(inf, f, indent=4)         
-                
+                json.dump(inf, f, indent=4)
+
         else:
             pass
 
         time.sleep(2)
-        
+
 
 
 if __name__ == '__main__':
---
+++ mytgapi.py
@@ -0,0 +1,91 @@
+import requests
+import json
+import random
+
+excuses = ('Что-то пошло не так...',
+            'Мне лень...',
+            'Рут не дописан...',
+            'А может лучше в памп поиграем?'
+            '*Смешная фраза для ответа на непредвиденные команды*',
+            'Ачивки тебе за это не дадут')
+
+URL = 'https://api.telegram.org/bot'
+with open('token', 'r') as f:
+    URL += f.read().strip()
+
+def getUpdates(update_id=1):
+    url = URL + '/getUpdates?offset=' + str(update_id)
+    r = requests.get(url)
+    return r.json()
+
+
+def sendMessage(chat_id, text="Test message"):
+    url = URL + '/sendMessage'
+    r = requests.post(url, json={'chat_id': chat_id, 'text': text})
+    return r.json()
+
+
+def getMe():
+    url = URL + '/getMe'
+    r = requests.get(url)
+    return r.json()
+
+
+def help_message():
+    string = "Все команды вводятся в формате /setcount [число]\n" +\
+    "Извините, я упячко (I will fix it)\n"+\
+    "/help - вывести список команд;\n" +\
+    "```/nick``` - задать себе имя\n" +\
+    "```/come``` - приехать на южку;\n" +\
+    "```/leave``` - уехать с южки;\n" +\
+    "/setcount - расскажите, сколько людей сейчас на пампе;\n" +\
+    "/info - узнать количество людей;\n"
+    return string
+
+
+def info_message(inf):
+    string = "Количество людей на южке: " + str(inf['count_of_people'])
+    return string
+
+
+def answerMessage(message, info):
+    msg_txt = message['message']['text']
+    chat_id = message['message']['chat']['id']
+    answer = None
+    inf = info
+
+    if '/start' in msg_txt:
+        answer = help_message()
+        pass
+
+    elif '/help' in msg_txt:
+        answer = help_message()
+        pass
+
+    elif '/come' in msg_txt:
+        #sendMessage(chat_id, help_message())
+        pass
+
+    elif '/leave' in msg_txt:
+        #Убрать из списка
+        pass
+
+
+    elif '/info' in msg_txt:
+        answer = info_message()
+        pass
+
+    elif '/setcount' in msg_txt:
+        if msg.split()[1].isdigit():
+            if 0 <= int(msg.split()[1]) <= 25:
+                inf['count_of_people'] = int(msg.split()[1])
+                answer = info_message()
+            else:
+                answer = 'Я программист, меня не обманешь... почти.'
+        else:
+            answer = "Неверный формат сообщения"
+
+    else:
+        answer = random.choice(excuses)
+
+    return answer, inf
