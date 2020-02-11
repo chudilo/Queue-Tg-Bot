20cab3f37c566eeb503f710e0307a9f68340a6f6
chudilo <chudik97@yandex.ru>
2020-02-11 17:35:54 +0000

    Working getUpdate

--- telegram_api.py
+++ telegram_api.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask import request
 
+import json
+import requests
+
 
 class TgBot(object):
     def __init__(self, token, commands):
         self.token = token
         self.requestUrl = 'https://api.telegram.org/bot' + token
         self.commands = commands
+        self.offset = 0
 
     def getMe(self):
         r = requests.get(self.requestUrl + '/getMe')
+        return r.json()
+
+    def getUpdates(self, timeout=0):
+        print("offset:", self.offset)
+        r = requests.post(self.requestUrl + '/getUpdates', json={'offset': self.offset,
+                                                                'timeout': timeout})
         return r.json()
 
     def sendMessage(self, chat_id, text, reply_markup=None):
@@ -18,24 +28,34 @@ class TgBot(object):
     def sendSticker(self):
         pass
 
-    def setWebHook(self):
-        pass
+    def handleMessage(self, message):
+        self.offset = message['update_id'] + 1
+        print("message", message)
 
-    def deleteWebHook(self):
-        pass
-
-    def handleMessage(self, message: 'json'):
-        pass
-
-    def run(self):
+    # TODO: Make web hook bot
+    def startWebHook(self):
         app = Flask(__name__)
 
         @app.route('/', methods=['POST'])
         def index():
             if request.method == 'POST':
                 r = request.data.decode('unicode-escape')  # .decode("ascii").encode("utf-8").decode('unicode-escape')
-                print(json.dumps(json.loads(r), ensure_ascii=False, indent=4))
+                # print(json.dumps(json.loads(r), ensure_ascii=False, indent=4))
+                print(r)
 
             return "correct"
 
         app.run(port='5000')
+
+    def startGetUpdate(self):
+        while True:
+            update = self.getUpdates(timeout=30)
+            for message in update['result']:
+                self.handleMessage(message)
+
+
+    def run(self, webHook=False):
+        if webHook:
+            self.startWebHook()
+        else:
+            self.startGetUpdate()
--- uzhka_bot.py
+++ uzhka_bot.py
@@ -2,25 +2,11 @@ import os
 from telegram_api import TgBot
 
 
-class UzhkaBot(TgBot):
-    def __init__(self, token, database):
-        commands = {}
-
-        super().__init__(token, commands)
-        self.database = database
-
-    def __start(self):
-        pass
-
-    def __info(self):
-        pass
-
-
 def main():
-    token = os.environ['TELEGRAM_TOKEN']
+    token = os.environ['TEST_TOKEN']
     database = "database.db"
 
-    bot = UzhkaBot(token, database)
+    bot = TgBot(token, database)
     bot.run()
 
 
